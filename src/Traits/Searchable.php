d74d35fdb909acd7590ff609e06e57c6f15e525d
EL MEHDI AIT FAKIR <elmehdiaitfakir@gmail.com>
2025-01-06 23:10:47 +0100

    Scope a query that searches for an exact term in the searchable fields

--- src/Traits/Searchable.php
+++ src/Traits/Searchable.php
@@ -23,6 +23,25 @@ trait Searchable
     }
 
     /**
+     * Scope a query that searches for an exact term in the searchable fields.
+     *
+     * This method is similar to the `search` scope, but it does not use LIKE
+     * operator. Instead, it searches for an exact value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $term
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSearchExact($query, $term): Builder
+    {
+        return $query->where(function (Builder $query) use ($term) {
+            foreach ($this->getSearchableFields() as $field) {
+                $query->orWhere($field, $term);
+            }
+        });
+    }
+
+    /**
      * Retrieve the list of fields that are searchable.
      *
      * @return array The array of searchable field names.
